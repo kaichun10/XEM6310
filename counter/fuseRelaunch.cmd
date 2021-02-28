cb545da7edeb9da7f4976a279d06115dc4b30a4e
kaichun <kaichunhu10@outlook.com>
2021-02-28 22:39:35 +0000

    counter+counter_tb

--- counter/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "C:/LocalXilinx/Projects/counter/counter/counter_tb_isim_beh.exe" -prj "C:/LocalXilinx/Projects/counter/counter/counter_tb_beh.prj" "work.counter_tb" 
